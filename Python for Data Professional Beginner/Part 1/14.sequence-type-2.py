d83bd15e5ed0527513c59debddf89aef5ab3520c
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:08:53 +0700

    feat: add sequence type part 2

---
+++ Python for Data Professional Beginner/Part 1/14.sequence-type-2.py
@@ -0,0 +1,5 @@
+contoh_tuple = ('Januari', 'Februari', 'Maret', 'April')
+print(contoh_tuple[0])
+contoh_tuple = ('Januari', 'Februari', 'Maret', 'April')
+contoh_tuple = list(contoh_tuple)
+contoh_tuple[0] = 'December'
