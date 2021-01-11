e432b8f5bbe28242dfbcc0cfa8424659791c9209
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:37:44 +0700

    feat: add Conditioning part 2 as part 1 has no answer

---
+++ Python for Data Professional Beginner/Part 1/27.conditioning-2.py
@@ -0,0 +1,14 @@
+# Statement if
+x = 4
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+    print("x habis dibagi dua") # statemen aksi lebih menjorok ke dalam
+# Statement if ... elif ... else
+x = 7
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+    print("x habis dibagi dua")
+elif x % 3 == 0: # jika sisa bagi x dengan 3 sama dengan 0
+    print("x habis dibagi tiga")
+elif x % 5 == 0: # jika sisa bagi x dengan 5 sama dengan 0
+    print("x habis dibagi lima")
+else:
+    print("x tidak habis dibagi dua, tiga ataupun lima")
