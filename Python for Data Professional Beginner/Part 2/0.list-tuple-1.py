14bf50b82e47fb807848492f4bc89d2a5c881f1e
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:53:36 +0700

    feat: add List Tuple part 1

---
+++ Python for Data Professional Beginner/Part 2/0.list-tuple-1.py
@@ -0,0 +1,5 @@
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+print(bulan_pembelian[0])
+print(bulan_pembelian[5])
+print(bulan_pembelian[-1])
+print(bulan_pembelian[-2])
