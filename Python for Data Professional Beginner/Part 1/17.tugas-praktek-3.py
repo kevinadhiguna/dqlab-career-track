e1dc1b8245c51b95f5a3f3979f14c46f65be0421
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:12:48 +0700

    feat: add third Tugas Praktek

---
+++ Python for Data Professional Beginner/Part 1/17.tugas-praktek-3.py
@@ -0,0 +1,3 @@
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000 }
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000 }
