c79254817c988363b36dc084bdebb9299cf41885
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:45:41 +0700

    feat: add For Loops part 1

---
+++ Python for Data Professional Beginner/Part 1/34.for-loops-1.py
@@ -0,0 +1,5 @@
+list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+total_tagihan = 0
+for tagihan in list_tagihan : # untuk setiap tagihan dalam list_tagihan
+    total_tagihan += tagihan # tambahkan tagihan ke total_tagihan
+print(total_tagihan)
