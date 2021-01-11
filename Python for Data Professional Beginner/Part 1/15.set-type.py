5d9262d8e65fa624d50f383fbe40800d427a3d6b
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:11:04 +0700

    feat: add Set type

---
+++ Python for Data Professional Beginner/Part 1/15.set-type.py
@@ -0,0 +1,6 @@
+contoh_list = ['Dewi', 'Budi', 'Cici', 'Linda', 'Cici']
+print(contoh_list)
+contoh_set = {'Dewi', 'Budi', 'Cici', 'Linda', 'Cici'}
+print(contoh_set)
+contoh_frozen_set = ({'Dewi', 'Budi', 'Cici', 'Linda', 'Cici'})
+print(contoh_frozen_set)
