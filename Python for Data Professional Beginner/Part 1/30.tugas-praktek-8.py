5a981bc1b5cd1deb681138f2d919a9ea6b5a113a
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:40:50 +0700

    feat: add a new Tugas Praktek

---
+++ Python for Data Professional Beginner/Part 1/30.tugas-praktek-8.py
@@ -0,0 +1,22 @@
+jam = 17
+tagihan_ke = 'Mr. Yoyo'
+warehousing = { 'harga_harian': 1000000, 'total_hari':15 } 
+cleansing = { 'harga_harian': 1500000, 'total_hari':10 } 
+integration = { 'harga_harian':2000000, 'total_hari':15 } 
+transform = { 'harga_harian':2500000, 'total_hari':10 }
+sub_warehousing = warehousing['harga_harian']*warehousing['total_hari'] 
+sub_cleansing = cleansing['harga_harian']*cleansing['total_hari'] 
+sub_integration = integration['harga_harian']*integration['total_hari'] 
+sub_transform = transform['harga_harian']*transform['total_hari']
+total_harga = sub_warehousing+sub_cleansing+sub_integration+sub_transform
+print("Tagihan kepada:")
+print(tagihan_ke)
+if jam > 19 :
+    print("Selamat malam, anda harus membayar tagihan sebesar:")
+elif jam > 17 :
+    print("Selamat sore, anda harus membayar tagihan sebesar:") 
+elif jam > 12 :
+    print("Selamat siang, anda harus membayar tagihan sebesar:")
+else:
+    print("Selamat pagi, anda harus membayar tagihan sebesar:") 
+print(total_harga)
