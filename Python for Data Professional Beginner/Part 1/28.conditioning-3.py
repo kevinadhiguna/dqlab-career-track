0b83187ffb31e95bc901b96f958cfbeb235c2ad2
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:38:41 +0700

    feat: add Conditioning part 3

---
+++ Python for Data Professional Beginner/Part 1/28.conditioning-3.py
@@ -0,0 +1,9 @@
+jam = 13
+if jam >= 5 and jam < 12: # selama jam di antara 5 s.d. 12
+    print("Selamat pagi!")
+elif jam >= 12 and jam < 17: # selama jam di antara 12 s.d. 17
+    print("Selamat siang!")
+elif jam >= 17 and jam < 19 : # selama jam di antara 17 s.d. 19
+    print("Selamat sore!")
+else: # selain kondisi di atas
+    print("Selamat malam!")
