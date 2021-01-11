88a4ae8c78c623eb56ba8f29f67707c8748bb0ae
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:47:19 +0700

    feat: add For Loops part 3

---
+++ Python for Data Professional Beginner/Part 1/36.for-loops-3.py
@@ -0,0 +1,5 @@
+list_daerah = ['Malang', 'Palembang', 'Medan']
+list_buah = ['Apel', 'Duku', 'Jeruk']
+for nama_daerah in list_daerah :
+    for nama_buah in list_buah :
+        print(nama_buah+" "+nama_daerah)
