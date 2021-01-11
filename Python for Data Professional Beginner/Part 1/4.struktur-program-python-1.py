6b5c44d64421327971f867ed8cf9ed1ffdd3a327
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-01-11 05:47:19 +0000

    feat: Struktur Program Python - part 1

---
+++ Python for Data Professional Beginner/Part 1/4.struktur-program-python-1.py
@@ -0,0 +1,10 @@
+# Statement
+print("Belajar Python menyenangkan") 
+print("Halo Dunia")
+print("Hello World!")
+# Variables & Literals
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+# Operators
+print(bilangan1 + bilangan2)
