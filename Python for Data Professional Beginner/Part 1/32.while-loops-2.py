7f36b4abf525e626117118a7294185e5c20733f3
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:43:57 +0700

    feat: add While Loops part 2

---
+++ Python for Data Professional Beginner/Part 1/32.while-loops-2.py
@@ -0,0 +1,14 @@
+tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+i = 0
+jumlah_tagihan = len(tagihan)
+total_tagihan = 0
+while i < jumlah_tagihan :
+    # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
+    # pengulangan akan dihentikan
+    if tagihan[i] < 0 :
+        total_tagihan = -1
+        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan += tagihan[i]
+    i += 1
+print(total_tagihan)
