a14d531fbc07de3bc436de296614f6328109d1c3
kevinadhiguna <kevin26798@gmail.com>
2021-01-11 13:43:04 +0700

    feat: add While Loops part 1

---
+++ Python for Data Professional Beginner/Part 1/31.while-loops-1.py
@@ -0,0 +1,13 @@
+# Tagihan
+tagihan = [50000, 75000, 125000, 300000, 200000]
+# Tanpa menggunakan while loop
+total_tagihan = tagihan[0] + tagihan[1] + tagihan[2] + tagihan[3] + tagihan[4]
+print(total_tagihan)
+# Dengan menggunakan while loop
+i = 0 # sebuah variabel untuk mengakses setiap elemen tagihan satu per satu
+jumlah_tagihan = len(tagihan) # panjang (jumlah elemen dalam) list tagihan
+total_tagihan = 0 # mula-mula, set total_tagihan ke 0
+while i < jumlah_tagihan : # selama nilai i kurang dari jumlah_tagihan
+    total_tagihan += tagihan[i] # tambahkan tagihan[i] ke total_tagihan
+    i += 1 # tambahkan nilai i dengan 1 untuk memproses tagihan selanjutnya.
+print(total_tagihan)
