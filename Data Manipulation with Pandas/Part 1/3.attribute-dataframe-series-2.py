cd9c1bbdaa55d8b1fe37c65ded4b763bbe9832c0
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 13:43:36 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 1/3.attribute-dataframe-series-2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# Series
+number_list = pd.Series([1,2,3,4,5,6])
+# DataFrame
+matrix_list = pd.DataFrame([[1,2,3],
+				            ['a','b','c'],
+				            [3,4,5],
+				            ['d',4,6]])
+# [5] attribute .copy()
+print("[5] attribute .copy()")
+num_list = number_list.copy()
+print("    Copy number_list ke num_list:", num_list)
+mtr_list = matrix_list.copy()
+print("    Copy matrix_list ke mtr_list:", mtr_list)	
+# [6] attribute .to_list()
+print("[6] attribute .to_list()")
+print(number_list.to_list())
+# [7] attribute .unique()
+print("[7] attribute .unique()")
+print(number_list.unique())
