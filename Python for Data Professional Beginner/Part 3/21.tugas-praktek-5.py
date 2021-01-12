e274ec77a62aff04a1cab198640a9ece03741e66
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:36:38 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/21.tugas-praktek-5.py
@@ -0,0 +1,32 @@
+class Karyawan: 
+    nama_perusahaan = 'ABC' 
+    insentif_lembur = 250000
+    # usia akan di-set nilainya menjadi 21 saat tidak
+    # dispesifikasikan dan pendapatan akan di-set nilainya
+    # menjadi 5000000 saat tidak dispesifikasikan
+    def __init__(self, nama, usia=21, pendapatan=5000000): 
+        self.nama = nama
+        self.usia = usia
+        self.pendapatan = pendapatan 
+        self.pendapatan_tambahan = 0
+    def lembur(self):
+        self.pendapatan_tambahan += self.insentif_lembur 
+    def tambahan_proyek(self, insentif_proyek):
+        self.pendapatan_tambahan += insentif_proyek 
+    def total_pendapatan(self):
+        return self.pendapatan + self.pendapatan_tambahan
+# Karyawan baru pertama yang bernama Budi
+karyawan_baru1 = Karyawan('Budi')
+print(karyawan_baru1.nama)
+print(karyawan_baru1.usia)
+print(karyawan_baru1.total_pendapatan())
+# Karyawan baru ke-2 yang bernama Didi, umur 25
+karyawan_baru2 = Karyawan('Didi', 25)
+print(karyawan_baru2.nama)
+print(karyawan_baru2.usia)
+print(karyawan_baru2.total_pendapatan())
+# Karyawan baru ke-3 yang bernama Hadi, pendapatan 8000000
+karyawan_baru3 = Karyawan('Hadi', pendapatan = 8000000)
+print(karyawan_baru3.nama)
+print(karyawan_baru3.usia)
+print(karyawan_baru3.total_pendapatan())
