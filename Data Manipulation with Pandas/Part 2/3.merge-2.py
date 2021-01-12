511a923d7965b45979be66cbcd95a5799980c418
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 03:59:09 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 2/3.merge-2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+}).set_index(['key', 'val2'])
+print('Dataframe 1:\n', df1)
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+}).set_index(['key', 'val3'])
+print('Dataframe 2:\n', df2)
+# Merge dataframe yang memiliki multi index
+df_merge = pd.merge(df1.reset_index(), df2.reset_index())
+print('Merging dataframe:\n', df_merge)
