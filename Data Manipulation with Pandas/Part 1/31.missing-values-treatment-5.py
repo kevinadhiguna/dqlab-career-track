e9786528c7a03c45844b0d5affc4798dbf8f941c
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 14:18:14 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 1/31.missing-values-treatment-5.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+# Data
+ts = pd.Series({
+   "2020-01-01":9,
+   "2020-01-02":np.nan,
+   "2020-01-05":np.nan,
+   "2020-01-07":24,
+   "2020-01-10":np.nan,
+   "2020-01-12":np.nan,
+   "2020-01-15":33,
+   "2020-01-17":np.nan,
+   "2020-01-16":40,
+   "2020-01-20":45,
+   "2020-01-22":52,
+   "2020-01-25":75,
+   "2020-01-28":np.nan,
+   "2020-01-30":np.nan
+})
+# Isi missing value menggunakan interpolasi linier
+ts = ts.interpolate()
+# Cetak time series setelah interpolasi linier
+print("Setelah diisi missing valuenya:\n", ts)
