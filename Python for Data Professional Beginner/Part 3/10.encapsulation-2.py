fb9073794577a9e65405570f4433417fa143b76f
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:26:54 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/10.encapsulation-2.py
@@ -0,0 +1,21 @@
+# Definisikan class Karyawan
+class Karyawan: 
+    nama_perusahaan = 'ABC' 
+    __insentif_lembur = 250000
+    def __init__(self, nama, usia, pendapatan): 
+        self.__nama = nama
+        self.__usia = usia 
+        self.__pendapatan = pendapatan 
+        self.__pendapatan_tambahan = 0
+    def lembur(self):
+        self.__pendapatan_tambahan += self.__insentif_lembur 
+    def tambahan_proyek(self, insentif_proyek):
+        self.__pendapatan_tambahan +=insentif_proyek 
+    def total_pendapatan(self):
+        return self.__pendapatan + self.__pendapatan_tambahan
+# Buat objek karyawan bernama Aksara
+aksara = Karyawan('Aksara', 25, 8500000)
+# Akses ke attribute class Karyawan
+print(aksara.__class__.nama_perusahaan)
+# Akan menimbulkan error ketika di run
+print(aksara.__nama)
