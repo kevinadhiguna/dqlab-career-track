7a0e61ec0ff8a8a71af77de585b796d9dd81f6d0
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:21:07 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/1.list-tuple-2.py
@@ -0,0 +1,8 @@
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+pertengahan_tahun = bulan_pembelian[4:8]
+print(pertengahan_tahun)
+awal_tahun = bulan_pembelian[:5]
+print(awal_tahun)
+akhir_tahun = bulan_pembelian[8:]
+print(akhir_tahun)
+print(bulan_pembelian[-4:-1])
