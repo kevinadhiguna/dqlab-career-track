c4fa1c51e7eeaebe9901e5c6afa7c15b1e0e5fe6
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:50:37 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/20.startswith-endswith-in-string.py
@@ -0,0 +1,9 @@
+# Fitur .startswith()
+print(">>> Fitur .startswith()")
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+print(teks.startswith("Apel"))
+print(teks.startswith("apel"))
+# Fitur .endswith()
+print(">>> Fitur .endswith()")
+print(teks.endswith("lainnya"))
+print(teks.endswith("apel"))
