ce49186365b87819f6a4109a6adf543bbcf0b9a6
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 04:18:12 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 2/23.convert-to-datetime.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# Load dataset https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+# Cetak 5 data teratas
+print('Sebelum diubah dalam format datetime:\n', gaq.head())
+# Ubah menjadi datetime
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+# Cetak 5 data teratas
+print('Sesudah diubah dalam format datetime:\n', gaq.head())
