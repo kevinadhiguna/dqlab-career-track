ac66a1c5abb7c66fe4e91b97776998b3e33b0c23
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:02:59 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/31.write-text-file-1.py
@@ -0,0 +1,5 @@
+# Menulis ke file hello_write.txt
+file = open("hello.txt", "w")
+file.write("Sekarang kita belajar menulis dengan menggunakan Python")
+file.write("Menulis konten file dengan mode w (write).")
+file.close()
