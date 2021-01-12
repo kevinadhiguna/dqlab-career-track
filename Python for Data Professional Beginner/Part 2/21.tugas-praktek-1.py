7ce0d7ad3175628c6a4126aec1ffec2efc98af6d
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:51:23 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/21.tugas-praktek-1.py
@@ -0,0 +1,21 @@
+judul_artikel = [
+"Buah Salak Baik untuk Mata", "Buah Salak Kaya Potasium", 
+"Buah Jeruk Kaya Vitamin C", "Buah Salak Kaya Manfaat", 
+"Salak Baik untuk Jantung", "Jeruk dapat Memperkuat Tulang", 
+"Jeruk Mencegah Penyakit Asma", "Jeruk Memperkuat Gigi", 
+"Jeruk Mencegah Kolesterol Jahat", "Salak Mencegah Diabetes", 
+"Salak Memperkuat Dinding Usus", "Salak Baik untuk Darah",
+"Jeruk Kaya Manfaat untuk Jantung", "Salak si Kecil yang Baik", 
+"Jeruk dan Salak Buah Kaya Manfaat", "Buah Jeruk Enak",
+"Tips Panen Jeruk Ribuan Kilo", "Tips Bertanam Salak", 
+"Salak Manis untuk Berbuka", "Jeruk Baik untuk Wajah"
+]
+jumlah_artikel_jeruk = 0
+jumlah_artikel_salak = 0
+for judul in judul_artikel:
+    if judul.count("Jeruk") > 0: 
+        jumlah_artikel_jeruk += 1
+    if judul.count("Salak") > 0:
+        jumlah_artikel_salak += 1
+print(jumlah_artikel_jeruk) 
+print(jumlah_artikel_salak)
