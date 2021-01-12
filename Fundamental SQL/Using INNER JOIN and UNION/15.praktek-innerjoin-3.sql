c97880114061839b95ce59e78f37bcef32d5a2ae
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 20:49:17 +0700

    feat: add a new Query

---
+++ Fundamental SQL/Using INNER JOIN and UNION/15.praktek-innerjoin-3.sql
@@ -0,0 +1,1 @@
+select tr_penjualan.kode_transaksi, tr_penjualan.kode_pelanggan, tr_penjualan.kode_produk, ms_produk.nama_produk, ms_produk.harga, tr_penjualan.qty, ms_produk.harga*tr_penjualan.qty as total from tr_penjualan inner join ms_produk on tr_penjualan.kode_produk = ms_produk.kode_produk;
