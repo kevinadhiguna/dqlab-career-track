dcbd54a43cb68e161498a2bb2d1964dbc4d2b555
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:48:12 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/17.modify-caps-in-string.py
@@ -0,0 +1,12 @@
+# Fitur .capitalize()
+print(">>> Fitur .capitalize()")
+judul_buku = 'belajar bahasa Python'
+print(judul_buku.capitalize())
+# Fitur .lower()
+print(">>> Fitur .lower()")
+judul_buku = 'Belajar Bahasa PYTHON.'
+print(judul_buku.lower())
+# Fitur .upper()
+print(">>> Fitur .upper()")
+judul_buku = 'Belajar Bahasa PYTHON.'
+print(judul_buku.upper())
