b320402fccc202a2bacbaace7e3d275fdd101e45
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:54:09 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/24.function-2.py
@@ -0,0 +1,6 @@
+# Definsikan fungsi 
+def fungsi_dengan_argumen(nama_depan, nama_belakang):
+    print(nama_depan+" "+ nama_belakang)
+# Panggil fungsi dengan memasukkan argumen
+# nama_depan yaitu "John" dan nama_belakang "Doe"
+fungsi_dengan_argumen("John", "Doe")
