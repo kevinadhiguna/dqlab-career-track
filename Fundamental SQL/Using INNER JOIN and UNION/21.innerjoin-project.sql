ae26833e9ed17918f5a409138e2e8689394a552c
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 21:04:27 +0700

    feat: add a new Quiz

---
+++ Fundamental SQL/Using INNER JOIN and UNION/21.innerjoin-project.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT ms_pelanggan.kode_pelanggan, ms_pelanggan.nama_customer, ms_pelanggan.alamat FROM ms_pelanggan INNER JOIN tr_penjualan ON ms_pelanggan.kode_pelanggan = tr_penjualan.kode_pelanggan WHERE tr_penjualan.nama_produk='Kotak Pensil DQLab' OR tr_penjualan.nama_produk='Flashdisk DQLab 32 GB' OR tr_penjualan.nama_produk='Sticky Notes DQLab 500 sheets';
