a8d95bb3f33e9f03672b2bde735fd0276608ccec
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:49:48 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/19.find-count-in-string.py
@@ -0,0 +1,9 @@
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+# Fitur .find()
+print(">>> Fitur .find()")
+print(teks.find("Apel"))
+print(teks.find("malang"))
+# Fitur .count()
+print(">>> Fitur .count()")
+kemunculan_kata_apel = teks.count("apel")
+print(kemunculan_kata_apel)
