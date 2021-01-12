feed715c4644306a8b028eba457c426814ab6f59
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 04:10:52 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 2/15.groupby-aggregation-with-statistics-function-1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Load data global_air_quality.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+# Create variabel pollutant 
+pollutant = gaq[['country', 'city', 'pollutant', 'value']].pivot_table(index=['country', 'city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+# [1] Group berdasarkan country dan terapkan aggregasi mean
+pollutant_mean = pollutant.groupby('country').mean()
+print('Rata-rata pollutant (5 teratas):\n', pollutant_mean.head())
+# [2] Group berdasarkan country dan terapkan aggregasi std
+pollutant_std = pollutant.groupby('country').std().fillna(0)
+print('Standar deviasi pollutant (5 teratas):\n', pollutant_std.head())
