67346ed4edef06e50b124a1b27fd43a7d532fa67
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 04:18:59 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 2/24.downsampling-data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Load dataset https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+print('Dataset sebelum di-downsampling (5 teratas):\n', gaq.head())
+# [1] Downsampling dari daily to weekly dan kita hitung maksimum untuk seminggu
+gaq_weekly = gaq.resample('W').max()
+print('Downsampling daily to weekly - max (5 teratas):\n', gaq_weekly.head())
+# [2] Downsampling dari daily to quaterly dan kita hitung minimumnya untuk tiap quarter
+gaq_quaterly = gaq.resample('Q').min()
+print('Downsampling daily to quaterly - min (5 teratas):\n', gaq_quaterly.head())
