b3eb70b0f223e355503fd787d308e8a1eb592f8a
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:08:57 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/35.math-library-1.py
@@ -0,0 +1,33 @@
+# Import library math
+import math
+# Fungsi math.ceil()
+print(">>> Fungsi math.ceil()")
+x = 10.32
+y = 13.87
+x_ceil = math.ceil(x)
+y_ceil = math.ceil(y)
+print(x_ceil)
+print(y_ceil)
+# Fungsi math.floor()
+print(">>> Fungsi math.floor()")
+x_floor = math.floor(x)
+y_floor = math.floor(y)
+print(x_floor)
+print(y_floor)
+# Fungsi math.fabs()
+print(">>> Fungsi math.fabs()")
+x = 10.32
+y = -13.87
+x = math.fabs(x)
+y = math.fabs(y)
+print(x)
+print(y)
+# Fungsi math.factorial()
+print(">>> Fungsi math.factorial()")
+x_factorial = math.factorial(5)
+print(x_factorial)
+# Fungsi math.fsum()
+print(">>> Fungsi math.fsum()")
+x = [1, 2, 3, 4, 5, 6, -6, -5, -4]
+total = math.fsum(x)
+print(total)
