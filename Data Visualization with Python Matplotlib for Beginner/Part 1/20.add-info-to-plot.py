c9cfdcb41f22d30edd282c7f208b8022884cc17c
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 03:49:02 +0700

    feat: add a new script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/20.add-info-to-plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=40, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.text(0.45, 0.72, 'The GMV increased significantly on October 2019', transform=fig.transFigure, color='red')
+plt.show()
