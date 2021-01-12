fdfd99c5c685708f4f5b5035aa1e0006d5257eb3
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:39:24 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/13.tugas-praktek-1.py
@@ -0,0 +1,16 @@
+# Data keuangan
+keuangan = {
+'pengeluaran': [2, 2.5, 2.25, 2.5, 3.2, 2.5, 3.5, 4, 3],
+'pemasukan': [7.8, 7.5, 9, 7.6, 7.2, 7.5, 7, 10, 7.5]
+}
+# Perhitungan rata-rata pemasukan dan rata-rata pengeluaran
+total_pengeluaran = 0
+total_pemasukan = 0
+for biaya in keuangan['pengeluaran'] : 
+    total_pengeluaran += biaya
+for biaya in keuangan['pemasukan']: 
+    total_pemasukan += biaya
+rata_rata_pengeluaran = total_pengeluaran / len(keuangan['pengeluaran'])
+rata_rata_pemasukan = total_pemasukan / len(keuangan['pemasukan'])
+print(rata_rata_pengeluaran) 
+print(rata_rata_pemasukan)
