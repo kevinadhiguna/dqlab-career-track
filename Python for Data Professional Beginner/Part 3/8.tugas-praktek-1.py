c1cf44901458e820bc9ef85fadd98d950f661fc1
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:24:32 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/8.tugas-praktek-1.py
@@ -0,0 +1,31 @@
+# Definisikan class Karyawan
+class Karyawan:
+    def __init__(self, nama, usia, pendapatan, insentif_lembur): 
+        self.nama = nama
+        self.usia = usia
+        self.pendapatan = pendapatan 
+        self.pendapatan_tambahan = 0
+        self.insentif_lembur = insentif_lembur
+    def lembur(self):
+        self.pendapatan_tambahan += self.insentif_lembur
+    def tambahan_proyek(self, jumlah_tambahan):
+        self.pendapatan_tambahan += jumlah_tambahan
+    def total_pendapatan(self):
+        return self.pendapatan + self.pendapatan_tambahan
+# Definisikan class Perusahaan
+class Perusahaan:
+    def __init__(self, nama, alamat, nomor_telepon): 
+        self.nama = nama
+        self.alamat = alamat 
+        self.nomor_telepon = nomor_telepon
+        self.list_karyawan = []
+    def aktifkan_karyawan(self, karyawan): 
+        self.list_karyawan.append(karyawan)
+    def nonaktifkan_karyawan(self, nama_karyawan): 
+        karyawan_nonaktif = None
+        for karyawan in self.list_karyawan:
+            if karyawan.nama == nama_karyawan: 
+                karyawan_nonaktif = karyawan
+                break
+        if karyawan_nonaktif is not None: 
+            self.list_karyawan.remove(karyawan_nonaktif)
