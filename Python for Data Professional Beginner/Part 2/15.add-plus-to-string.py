e709156a2e328870c19cee94aadea2f998a82685
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:42:52 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/15.add-plus-to-string.py
@@ -0,0 +1,8 @@
+nama_depan = 'John'
+nama_belakang = 'Doee'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+print(nama_lengkap)
+umur = '27 tahun'
+alamat = 'Jl. Anggrek No. 100'
+nama_umur_alamat = 'Hi, saya ' + nama_lengkap + ' umur ' + umur + ', tinggal di ' + alamat + '.'
+print(nama_umur_alamat)
