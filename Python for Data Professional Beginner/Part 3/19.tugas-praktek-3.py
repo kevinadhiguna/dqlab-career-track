b5683b3f45285a398af65297902517d6f4c34c1a
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:34:51 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/19.tugas-praktek-3.py
@@ -0,0 +1,21 @@
+# Definisikan class Karyawan
+class Karyawan:
+    def __init__(self, nama, usia, pendapatan, insentif_lembur): 
+        self.nama = nama
+        self.usia = usia 
+        self.pendapatan = pendapatan 
+        self.pendapatan_tambahan = 0
+        self.insentif_lembur = insentif_lembur 
+    def lembur(self):
+        self.pendapatan_tambahan += self.insentif_lembur 
+    def tambahan_proyek(self,jumlah_tambahan):
+        self.pendapatan_tambahan += jumlah_tambahan 
+    def total_pendapatan(self):
+        return self.pendapatan + self.pendapatan_tambahan
+# Definisikan class TenagaLepas sebagai child class dari
+# class Karyawan
+class TenagaLepas(Karyawan):
+    def __init__(self, nama, usia, pendapatan):
+        super().__init__(nama, usia, pendapatan, 0)
+    def tambahan_proyek(self, nilai_proyek):
+        self.pendapatan_tambahan += nilai_proyek * 0.01
