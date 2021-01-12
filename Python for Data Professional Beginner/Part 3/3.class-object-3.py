57b5939360c88c267131167dd15dc77bef55b3f5
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:20:21 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/3.class-object-3.py
@@ -0,0 +1,14 @@
+# Definisikan class Karyawan
+class Karyawan:
+    nama_perusahaan = 'ABC'
+# Inisiasi object yang dinyatakan dalam variabel aksara dan senja
+aksara = Karyawan()
+senja = Karyawan()
+# Cetak nama perusahaan melalui penggunaan keyword __class__
+# pada class attribute nama_perusahaan
+print(aksara.__class__.nama_perusahaan)
+# Ubah nama_perusahaan menjadi 'DEF'
+aksara.__class__.nama_perusahaan = 'DEF'
+# Cetak nama_perusahaan objek aksara dan senja
+print(aksara.__class__.nama_perusahaan)
+print(senja.__class__.nama_perusahaan)
