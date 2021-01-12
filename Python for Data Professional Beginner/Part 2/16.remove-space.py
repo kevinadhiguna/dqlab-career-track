4f94490d33132667cd7bc7aa8b5d7283bbe372c3
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:47:19 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/16.remove-space.py
@@ -0,0 +1,15 @@
+# Fitur .strip()
+print(">>> Fitur .strip()")
+kata_sambutan = ' halo, selamat siang!   '
+kata_sambutan = kata_sambutan.strip()
+print(kata_sambutan)
+# Fitur .lstrip()
+print(">>> Fitur .lstrip()")
+kata_sambutan = ' halo, selamat siang!   '
+kata_sambutan = kata_sambutan.lstrip()
+print(kata_sambutan)
+# Fitur .rstrip()
+print(">>> Fitur .rstrip()")
+kata_sambutan = ' halo, selamat siang!   '
+kata_sambutan = kata_sambutan.rstrip()
+print(kata_sambutan)
