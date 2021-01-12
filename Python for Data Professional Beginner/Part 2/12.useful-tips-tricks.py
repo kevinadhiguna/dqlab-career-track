4bd92dcf2eb08b10855829dd06a07b15859db112
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:37:30 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/12.useful-tips-tricks.py
@@ -0,0 +1,18 @@
+# Tuple
+print(">>> Tuple")
+tuple_menu = ('Gado-gado','Ayam Goreng','Rendang','Ketoprak')
+jumlah_menu = len(tuple_menu)
+print(jumlah_menu)
+# List
+print(">>> List")
+list_menu = ['Gado-gado','Ayam Goreng','Rendang','Ketoprak']
+jumlah_menu = len(list_menu)
+print(jumlah_menu)
+# Konversi tipe data
+print(">>> Konversi tipe data")
+list_buah = ['Apel', 'Apel', 'Jeruk', 'Markisa', 'Jeruk', 'Markisa', 'Apel']
+set_buah = set(list_buah)
+print(set_buah)
+list_buah = list(set_buah)
+list_buah.sort()
+print(list_buah)
