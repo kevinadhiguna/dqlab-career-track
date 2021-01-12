658f58711cf41bea2ed212b814b52b2ad26739c7
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:25:15 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/9.tugas-praktek-2.py
@@ -0,0 +1,10 @@
+# Definisikan perusahaan
+perusahaan = Perusahaan('ABC', 'Jl. Jendral Sudirman, Blok 11', '(021)95205XX')
+# Definisikan nama-nama karyawan
+karyawan_1 = Karyawan('Ani', 25, 8500000, 100000)
+karyawan_2 = Karyawan('Budi', 28, 12000000, 150000)
+karyawan_3 = Karyawan('Cici', 30, 15000000, 200000)
+# Aktifkan karyawan di perusahaan ABC
+perusahaan.aktifkan_karyawan(karyawan_1) 
+perusahaan.aktifkan_karyawan(karyawan_2) 
+perusahaan.aktifkan_karyawan(karyawan_3)
