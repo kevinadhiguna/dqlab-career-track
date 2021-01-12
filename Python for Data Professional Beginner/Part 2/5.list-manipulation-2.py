2f5aca2e58965f87c666820c44cd846f3511b0ca
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:30:11 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/5.list-manipulation-2.py
@@ -0,0 +1,32 @@
+# Fitur .index()
+print(">>> Fitur .index()")
+list_score = ['Budi','Sud','Budi','Budi','Budi','Sud','Sud']
+score_pertama_sud = list_score.index('Sud') + 1
+print(score_pertama_sud) # akan menampilkan output 2
+# Fitur .insert()
+print(">>> Fitur .insert()")
+list_score = ['Budi','Sud','Budi','Budi','Sud']
+list_score.insert(3, 'Sud')
+print(list_score)
+# Fitur .pop()
+print(">>> Fitur .pop()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_menu.pop(1)
+print(list_menu)
+# Fitur .remove()
+print(">>> Fitur .remove()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.remove('Rendang')
+print(list_menu)
+# Fitur .reverse()
+print(">>> Fitur .reverse()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.reverse()
+print(list_menu)
+# Fitur .sort()
+print(">>> Fitur .sort()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.sort() # Default: Ascending
+print(list_menu) 
+list_menu.sort(reverse=True)# Descending
+print(list_menu) 
