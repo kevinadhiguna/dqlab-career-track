2a0b8b4d55eb879f861151c06c38546413a0a3a5
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:42:00 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/14.string-manipulation.py
@@ -0,0 +1,6 @@
+nama_produk = "Sepatu Niko"
+nama_produk = nama_produk[:2] + "P" + nama_produk[3:9] + "K" + nama_produk[-1]
+print(nama_produk)
+print(nama_produk[:7])
+print(nama_produk[7:])
+print(len(nama_produk))
