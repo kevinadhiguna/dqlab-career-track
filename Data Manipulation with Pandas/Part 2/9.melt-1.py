082b817bbfb0f7091e491fe17164a9dd506e81b8
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 04:05:24 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 2/9.melt-1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+# Pivoting dataframe
+data_pivot = data.pivot_table(index='kelas', columns='pelajaran', values='nilai', aggfunc='mean').reset_index()
+print('Pivoting dataframe:\n', data_pivot)
+# [1] Melting dataframe data_pivot
+data_melt_1 = pd.melt(data_pivot)
+print('Melting dataframe:\n', data_melt_1)
+# [2] Melting dataframe data_pivot dengan id_vars
+data_melt_2 = pd.melt(data_pivot, id_vars='kelas')
+print('Melting dataframe dengan idvars:\n', data_melt_2)
