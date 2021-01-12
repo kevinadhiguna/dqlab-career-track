c6fe9b7fb13464683512a38cceb82a92e7395d1c
kevinadhiguna <kevin26798@gmail.com>
2021-01-13 04:07:42 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 2/12.stack-and-unstack-2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+data = data.set_index(['kelas','murid','pelajaran'])
+data_unstack = data.unstack(level=1)
+print('Dataframe:\n', data_unstack)
+# [1] Stacking dataframe
+data_stack = data_unstack.stack()
+print('Stacked dataframe:\n', data_stack)
+# [2] Tukar posisi index setelah stacking dataframe
+data_swap = data_stack.swaplevel(1,2)
+print('Swapped data:\n', data_swap)
+# [3] Melakukan sort_index pada stacking dataframe
+data_sort = data_swap.sort_index()
+print('Sorted data:\n', data_sort)
