e54a850d1e278acff670c5a3b3c7ab3e17c0fde2
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 15:30:53 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 2/6.tuple-manipulation.py
@@ -0,0 +1,12 @@
+# Fitur .count()
+print(">>> Fitur .count()")
+tuple_score = ('Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud')
+score_budi = tuple_score.count('Budi')
+score_sud = tuple_score.count('Sud')
+print(score_budi) # akan menampilkan output 4
+print(score_sud) # akan menampilkan output 3
+# Fitur .index()
+print(">>> Fitur .index()")
+tuple_score = ('Budi','Sud','Budi','Budi','Budi','Sud','Sud')
+score_pertama_sud = tuple_score.index('Sud')+1
+print(score_pertama_sud) # akan menampilkan output 2
