1ee32c4cb40d1e87d977fc21242cd90ef092aac2
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:29:48 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/13.inheritance-1.py
@@ -0,0 +1,35 @@
+# Definisikan class Karyawan (sebagai base class)
+class Karyawan: 
+    nama_perusahaan = 'ABC' 
+    insentif_lembur = 250000
+    def __init__(self, nama, usia, pendapatan): 
+        self.nama = nama
+        self.usia = usia 
+        self.pendapatan = pendapatan 
+        self.pendapatan_tambahan = 0
+    def lembur(self):
+        self.pendapatan_tambahan += self.insentif_lembur 
+    def tambahan_proyek(self, insentif_proyek):
+        self.pendapatan_tambahan += insentif_proyek 
+    def total_pendapatan(self):
+        return self.pendapatan + self.pendapatan_tambahan
+# Buat class turunan (sebagai inherit class) dari class karyawan, 
+# yaitu class AnalisData
+class AnalisData(Karyawan):
+    def __init__(self, nama, usia, pendapatan):
+    # melakukan pemanggilan konstruktur class Karyawan 
+        super().__init__(nama, usia, pendapatan)
+# Buat kembali class turunan (sebagai inherit class) dari class karyawan,  
+# yaitu class IlmuwanData
+class IlmuwanData(Karyawan):
+    def __init__(self, nama, usia, pendapatan):
+        # melakukan pemanggilan konstruktur class Karyawan 
+        super().__init__(nama, usia, pendapatan)
+# Buat objek karyawan yang bekerja sebagai AnalisData
+aksara = AnalisData('Aksara', 25, 8500000)
+aksara.lembur()
+print(aksara.total_pendapatan())
+# Buat objek karyawan yang bekerja sebagai IlmuwanData
+senja = IlmuwanData('Senja', 28, 13000000)
+senja.tambahan_proyek(2000000)
+print(senja.total_pendapatan())
