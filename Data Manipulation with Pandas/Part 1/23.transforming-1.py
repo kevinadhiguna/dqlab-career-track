ec99f6027328f400a26f571b223c3c718ae5fa28
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 14:05:26 +0700

    feat: add a new script

---
+++ Data Manipulation with Pandas/Part 1/23.transforming-1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# Baca file sample_csv.csv
+df = pd.read_csv("https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/sample_csv.csv")
+# Tampilkan tipe data
+print("Tipe data df:\n", df.dtypes)
+# Ubah tipe data kolom order_date menjadi datetime
+df["order_date"] = pd.to_datetime(df["order_date"])
+# Tampilkan tipe data df setelah transformasi
+print("\nTipe data df setelah transformasi:\n", df.dtypes)
