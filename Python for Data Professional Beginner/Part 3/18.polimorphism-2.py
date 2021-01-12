3bb14296c95fae7823ee5ff055f92a7a520a6cf0
kevinadhiguna <kevin26798@gmail.com>
2021-01-12 16:34:09 +0700

    feat: add a new script

---
+++ Python for Data Professional Beginner/Part 3/18.polimorphism-2.py
@@ -0,0 +1,31 @@
+# Definisikan class Karyawan (sebagai base class)
+class Karyawan: 
+    nama_perusahaan = 'ABC' 
+    insentif_lembur = 250000
+    def __init__(self, nama, usia, pendapatan): 
+        self.nama = nama
+        self.usia = usia 
+        self.pendapatan = pendapatan 
+        self.pendapatan_tambahan = 0
+    def lembur(self):
+        self.pendapatan_tambahan += self.insentif_lembur 
+    def tambahan_proyek(self, insentif_proyek):
+        self.pendapatan_tambahan += insentif_proyek 
+    def total_pendapatan(self):
+        return self.pendapatan + self.pendapatan_tambahan
+# Buat class turunan (sebagai inherit class) dari class karyawan, 
+# yaitu class AnalisData
+class AnalisData(Karyawan):
+    def __init__(self, nama, usia, pendapatan): 
+        super().__init__ (nama, usia, pendapatan)
+    # mendefinisikan kembali fungsi lembur() pada class AnalisData 
+    def lembur(self):
+        # memanggil fungsi lembur pada class Karyawan 
+        super().lembur()
+        # pendapatan tambahan pada class AnalisData sebesar
+        # 5 % dari pendapatannya.
+        self.pendapatan_tambahan += int(self.pendapatan * 0.05)
+# Buat objek karyawan yang bekerja sebagai AnalisData
+aksara = AnalisData('Aksara', 25, 8500000)
+aksara.lembur()
+print(aksara.total_pendapatan())
