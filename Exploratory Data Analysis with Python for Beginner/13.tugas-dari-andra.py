daa6389220910f58cb7e2f9f30a9cf8a24b3a409
kevinadhiguna <kevin26798@gmail.com>
2021-01-16 23:01:11 +0700

    feat: add a new script

---
+++ Exploratory Data Analysis with Python for Beginner/13.tugas-dari-andra.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+order_df = pd.read_csv("https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/order.csv")
+# Median price yang dibayar customer dari masing-masing metode pembayaran. 
+median_price = order_df["price"].groupby(order_df["payment_type"]).median()
+print(median_price)
+# Ubah freight_value menjadi shipping_cost dan cari shipping_cost 
+# termahal dari data penjualan tersebut menggunakan sort.
+order_df.rename(columns={"freight_value": "shipping_cost"}, inplace=True)
+sort_value = order_df.sort_values(by="shipping_cost", ascending=0)
+print(sort_value)
+# Untuk product_category_name, berapa  rata-rata weight produk tersebut 
+# dan standar deviasi mana yang terkecil dari weight tersebut, 
+mean_value = order_df["product_weight_gram"].groupby(order_df["product_category_name"]).mean()
+print(mean_value)
+std_value = order_df["product_weight_gram"].groupby(order_df["product_category_name"]).std()
+print(std_value)
+# Buat histogram quantity penjualan dari dataset tersebutuntuk melihat persebaran quantity 
+# penjualan tersebut dengan bins = 5 dan figsize= (4,5)
+order_df[["quantity"]].hist(figsize=(4, 5), bins=5)
+plt.show()
