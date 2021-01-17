58d1fac3ffec657e1965e13b8691a53029177814
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:26:37 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/0.importing-data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import io
+import pandas_profiling
+retail_raw = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced_data_quality.csv')
