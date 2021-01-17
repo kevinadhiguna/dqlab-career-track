358e7b4db662b2dd2e37867b49ddda3325c11549
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:57:00 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/10.outliers.py
@@ -0,0 +1,13 @@
+# Q1, Q3, dan IQR
+Q1 = retail_raw['quantity'].quantile(0.25)
+Q3 = retail_raw['quantity'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Check ukuran (baris dan kolom) sebelum data yang outliers dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Removing outliers
+retail_raw = retail_raw[~((retail_raw['quantity'] < (Q1 - 1.5 * IQR)) | (retail_raw['quantity'] > (Q3 + 1.5 * IQR)))]
+
+# Check ukuran (baris dan kolom) setelah data yang outliers dibuang
+print('Shape akhir: ', retail_raw.shape)
