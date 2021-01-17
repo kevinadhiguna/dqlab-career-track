b076f40f7232b4914fa41e65970994438690c77a
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:32:22 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/2.descriptive-statistics-1.py
@@ -0,0 +1,7 @@
+# Kolom city
+length_city = len(retail_raw['city'])
+print('Length kolom city:', length_city)
+
+# Tugas Praktek: Kolom product_id
+length_product_id = len(retail_raw['product_id'])
+print('Length kolom product_id:', length_product_id)
