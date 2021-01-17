da5b67be12e2cc995618376b8dd0118bb2bd0e34
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:44:58 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/6.descriptive-statistics-5.py
@@ -0,0 +1,8 @@
+# Quantile statistics kolom quantity
+print('Kolom quantity:')
+print(retail_raw['quantity'].quantile([0.25, 0.5, 0.75]))
+
+# Tugas praktek: Quantile statistics kolom item_price
+print('')
+print('Kolom item_price:')
+print(retail_raw['item_price'].quantile([0.25, 0.5, 0.75]))
