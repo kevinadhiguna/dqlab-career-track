46a3e4c51aab9785f41d5ae7eee3d21557c06b9b
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 06:34:08 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/13.barchart.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.clf()
+dataset_dki_q4.groupby('city')['gmv'].sum().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('GMV Per City - DKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
+plt.xlabel('City', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation=0)
+plt.show()
