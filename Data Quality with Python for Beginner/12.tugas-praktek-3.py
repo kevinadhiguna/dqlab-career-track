6a934068837dcdb29b39e52e237af87eda2d9695
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:59:46 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/12.tugas-praktek-3.py
@@ -0,0 +1,8 @@
+# Check ukuran (baris dan kolom) sebelum data duplikasi dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Buang data yang terduplikasi
+retail_raw.drop_duplicates(inplace=True)
+
+# Check ukuran (baris dan kolom) setelah data duplikasi dibuang
+print('Shape akhir: ', retail_raw.shape)
