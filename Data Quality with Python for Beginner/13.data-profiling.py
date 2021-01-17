b0a85c9c1e010920f799b59960f052aff9088e52
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 16:42:14 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/13.data-profiling.py
@@ -0,0 +1,29 @@
+# Baca dataset uncleaned_raw.csv
+uncleaned_raw = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/uncleaned_raw.csv')
+
+#inspeksi dataframe uncleaned_raw
+print('Lima data teratas:') 
+print(uncleaned_raw.head())
+
+#Check kolom yang mengandung missing value
+print('\nKolom dengan missing value:') 
+print(uncleaned_raw.isnull().any())
+
+#Persentase missing value
+length_qty = len(uncleaned_raw['Quantity'])
+count_qty = uncleaned_raw['Quantity'].count()
+
+#mengurangi length dengan count
+number_of_missing_values_qty = length_qty - count_qty
+
+#mengubah ke bentuk float
+float_of_missing_values_qty = float(number_of_missing_values_qty / length_qty) 
+
+#mengubah ke dalam bentuk persen
+pct_of_missing_values_qty = '{0:.1f}%'.format(float_of_missing_values_qty*100) 
+
+#print hasil percent dari missing value
+print('Persentase missing value kolom Quantity:', pct_of_missing_values_qty)
+
+#Mengisi missing value tersebut dengan mean dari kolom tersebut
+uncleaned_raw['Quantity'] = uncleaned_raw['Quantity'].fillna(uncleaned_raw['Quantity'].mean())
