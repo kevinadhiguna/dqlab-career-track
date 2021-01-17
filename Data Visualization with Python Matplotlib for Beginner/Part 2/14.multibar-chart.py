662ecc8b02162194e8e8568d9557917c66732dcf
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 06:41:49 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/14.multibar-chart.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+dataset_dki_q4.groupby(['city', 'order_month'])['gmv'].sum().unstack().plot(kind='bar')
+plt.title('GMV Per City, Breakdown by Month\nDKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
+plt.xlabel('Province', fontsize=12)
+plt.ylabel('Total Amount (in Billions)', fontsize=12)
+plt.legend(bbox_to_anchor=(1,1), shadow=True, title='Month')
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
