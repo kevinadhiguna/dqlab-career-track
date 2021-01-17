a9b3842ca9e76ba7cb67d199b0048e7b75428b91
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:43:14 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/5.descriptive-statistics-4.py
@@ -0,0 +1,17 @@
+# Deskriptif statistics kolom quantity
+print('Kolom quantity')
+print('Minimum value: ', retail_raw['quantity'].min())
+print('Maximum value: ', retail_raw['quantity'].max())
+print('Mean value: ', retail_raw['quantity'].mean())
+print('Mode value: ', retail_raw['quantity'].mode())
+print('Median value: ', retail_raw['quantity'].median())
+print('Standard Deviation value: ', retail_raw['quantity'].std())
+
+# Tugas praktek: Deskriptif statistics kolom item_price
+print('')
+print('Kolom item_price')
+print('Minimum value: ', retail_raw['item_price'].min())
+print('Maximum value: ', retail_raw['item_price'].max())
+print('Mean value: ', retail_raw['item_price'].mean())
+print('Median value: ', retail_raw['item_price'].median())
+print('Standard Deviation value: ', retail_raw['item_price'].std())
