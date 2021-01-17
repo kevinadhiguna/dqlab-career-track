0d715e75caf39df11611f9bc37a9737af4ed1d88
kevinadhiguna <kevin26798@gmail.com>
2021-01-17 15:35:20 +0700

    feat: add a script

---
+++ Data Quality with Python for Beginner/3.descriptive-statistics-2.py
@@ -0,0 +1,7 @@
+# Count kolom city
+count_city = retail_raw['city'].count()
+print('Count kolom count_city:', count_city)
+
+# Tugas praktek: count kolom product_id
+count_product_id = retail_raw['product_id'].count()
+print('Count kolom product_id:', count_product_id)
