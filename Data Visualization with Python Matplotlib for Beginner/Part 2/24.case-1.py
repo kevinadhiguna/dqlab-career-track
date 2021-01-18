98ecab3cb592efd24f79490ebd0c61b716fc1cbb
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 22:20:47 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/24.case-1.py
@@ -0,0 +1,12 @@
+#mengambil informasi top 5 brands berdasarkan quantity
+top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
+                .sum()
+                .reset_index()
+                .sort_values(by='quantity',ascending=False)
+                .head(5))
+
+#membuat dataframe baru, filter hanya di bulan Desember 2019 dan hanya top 5 brands
+dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
+
+# print top brands
+print(top_brands)
