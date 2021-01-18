a4ca6241597b6c3db54e052f2452612bf81e37c1
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 22:39:20 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/25.case-2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+dataset_top5brand_dec.groupby(['order_date','brand'])['quantity'].sum().unstack().plot(marker='.', cmap='plasma')
+plt.title('Daily Sold Quantity Dec 2019 - Breakdown by Brands',loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('Order Date', fontsize = 12)
+plt.ylabel('Quantity',fontsize = 12)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+plt.legend(loc='upper center', bbox_to_anchor=(1.1, 1), shadow=True, ncol=1)
+plt.annotate('Terjadi lonjakan', xy=(7, 310), xytext=(8, 300),
+             weight='bold', color='red',
+             arrowprops=dict(arrowstyle='->',
+                             connectionstyle="arc3",
+                             color='red'))
+plt.gcf().set_size_inches(10, 5)
+plt.tight_layout()
+plt.show()
