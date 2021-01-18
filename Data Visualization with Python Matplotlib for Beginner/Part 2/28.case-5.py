0246f18f4706ebb1cf0ed334af0b1029c81ea4d2
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 22:47:38 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/28.case-5.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,5))
+plt.hist(dataset_top5brand_dec.groupby('product_id')['item_price'].median(), bins=10, stacked=True, range=(1,2000000), color='green')
+plt.title('Distribution of Price Median per Product\nTop 5 Brands in Dec 2019',fontsize=15, color='blue')
+plt.xlabel('Price Median', fontsize = 12)
+plt.ylabel('Number of Products',fontsize = 12)
+plt.xlim(xmin=0,xmax=2000000)
+plt.show()
