9d213fa435c2880612ee9a26779fdc23d61cf77a
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 12:52:34 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/15.stacked-chart.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+dataset_dki_q4.groupby(['order_month', 'city'])['gmv'].sum().sort_values(ascending=False).unstack().plot(kind='bar', stacked=True)
+plt.title('GMV Per Month, Breakdown by City\nDKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
+plt.xlabel('Order Month', fontsize=12)
+plt.ylabel('Total Amount (in Billions)', fontsize=12)
+plt.legend(bbox_to_anchor=(1, 1), shadow=True, ncol=1, title='City')
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
