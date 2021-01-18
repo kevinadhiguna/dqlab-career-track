8177776fb4b8d48678c6efb9939d94552d6b91d9
kevinadhiguna <kevin26798@gmail.com>
2021-01-18 22:41:52 +0700

    feat: add a script

---
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/26.case-3.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+plt.clf()
+dataset_top5brand_dec.groupby('brand')['product_id'].nunique().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('Number of Sold Products per Brand, December 2019',loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('Brand', fontsize = 15)
+plt.ylabel('Number of Products',fontsize = 15)
+plt.ylim(ymin=0)
+plt.xticks(rotation=0)
+plt.show()
