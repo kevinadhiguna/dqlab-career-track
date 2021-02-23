3598f31554c0d1a3022f3042d32dc4c30e2fbd32
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 09:53:30 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/17.convert-string-to-numeric.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+# Convert feature/column 'Month'
+LE = LabelEncoder()
+dataset['Month'] = LE.fit_transform(dataset['Month'])
+print(LE.classes_)
+print(np.sort(dataset['Month'].unique()))
+print('')
+
+# Convert feature/column 'VisitorType'
+LE = LabelEncoder()
+dataset['VisitorType'] = LE.fit_transform(dataset['VisitorType'])
+print(LE.classes_)
+print(np.sort(dataset['VisitorType'].unique()))
