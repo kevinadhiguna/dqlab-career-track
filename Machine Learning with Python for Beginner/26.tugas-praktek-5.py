568a9f4a4396362c8d19328aa044435f06115661
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 11:53:59 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/26.tugas-praktek-5.py
@@ -0,0 +1,31 @@
+#load dataset
+import pandas as pd
+housing = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/housing_boston.csv')
+#Data rescaling
+from sklearn import preprocessing
+data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+housing[['RM','LSTAT','PTRATIO','MEDV']] = data_scaler.fit_transform(housing[['RM','LSTAT','PTRATIO','MEDV']])
+# getting dependent and independent variables
+X = housing.drop(['MEDV'], axis = 1)
+y = housing['MEDV']
+# checking the shapes
+print('Shape of X:', X.shape)
+print('Shape of y:', y.shape)
+
+# splitting the data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+# checking the shapes  
+print('Shape of X_train :', X_train.shape)
+print('Shape of y_train :', y_train.shape)
+print('Shape of X_test :', X_test.shape)
+print('Shape of y_test :', y_test.shape)
+
+##import regressor from Scikit-Learn
+from sklearn.linear_model import LinearRegression
+# Call the regressor
+reg = LinearRegression()
+# Fit the regressor to the training data  
+reg = reg.fit(X_train, y_train)
+# Apply the regressor/model to the test data  
+y_pred = reg.predict(X_test)
