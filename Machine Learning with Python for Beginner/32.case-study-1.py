218a6f75e7a68a90a4cc316006c974b35a03373e
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 12:13:02 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/32.case-study-1.py
@@ -0,0 +1,16 @@
+#import library 
+import pandas as pd
+
+# Baca data 'ecommerce_banner_promo.csv'
+data = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/ecommerce_banner_promo.csv')
+
+#1. Data eksplorasi dengan head(), info(), describe(), shape
+print("\n[1] Data eksplorasi dengan head(), info(), describe(), shape")
+print("Lima data teratas:")
+print(data.head())
+print("Informasi dataset:")
+print(data.info())
+print("Statistik deskriptif dataset:")
+print(data.describe())
+print("Ukuran dataset:")
+print(data.shape)
