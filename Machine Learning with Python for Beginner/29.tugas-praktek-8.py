458bfe51d6b0a5db26d7ffa24da38480de5d4c95
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 12:04:40 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/29.tugas-praktek-8.py
@@ -0,0 +1,22 @@
+#import library
+import matplotlib.pyplot as plt
+
+#convert dataframe to array
+X = X.values
+#Separate X to xs and ys --> use for chart axis
+xs = X[:,0]
+ys = X[:,1]
+# Make a scatter plot of xs and ys, using labels to define the colors
+plt.scatter(xs,ys,c=labels, alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = cluster_model.cluster_centers_
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+# Make a scatter plot of centroids_x and centroids_y
+plt.scatter(centroids_x,centroids_y,marker='D', s=50)
+plt.title('K Means Clustering', fontsize = 20)
+plt.xlabel('Annual Income')
+plt.ylabel('Spending Score')
+plt.show()
