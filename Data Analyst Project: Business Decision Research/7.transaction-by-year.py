7d83f85072f50becd2ea8ec419ddc63b9b4311d6
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 16:15:28 +0700

    feat: add a script

---
+++ Data Analyst Project: Business Decision Research/7.transaction-by-year.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.clf()
+df_year = df.groupby(['Year_First_Transaction'])['Count_Transaction'].sum()
+df_year.plot(x='Year_First_Transaction', y='Count_Transaction', kind='bar', title='Graph of Transaction Customer')
+plt.xlabel('Year_First_Transaction')
+plt.ylabel('Num_of_Transaction')
+plt.tight_layout()
+plt.show()
