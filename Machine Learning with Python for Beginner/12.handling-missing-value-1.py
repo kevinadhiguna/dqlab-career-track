53c720fec7e59cc8e6f88a3b1cc57742edb0edb8
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 09:35:16 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/12.handling-missing-value-1.py
@@ -0,0 +1,6 @@
+#checking missing value for each feature  
+print('Checking missing value for each feature:')
+print(dataset.isnull().sum())
+#Counting total missing value
+print('\nCounting total missing value:')
+print(dataset.isnull().sum().sum())
