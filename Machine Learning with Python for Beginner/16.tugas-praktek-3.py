84883063cdfddd84e29b9e774fb159d75cbbbaea
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 09:49:26 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/16.tugas-praktek-3.py
@@ -0,0 +1,9 @@
+from sklearn.preprocessing import MinMaxScaler  
+#Define MinMaxScaler as scaler  
+scaler = MinMaxScaler()  
+#list all the feature that need to be scaled  
+scaling_column = ['Administrative','Administrative_Duration','Informational','Informational_Duration','ProductRelated','ProductRelated_Duration','BounceRates','ExitRates','PageValues']
+#Apply fit_transfrom to scale selected feature  
+dataset[scaling_column] = scaler.fit_transform(dataset[scaling_column])
+#Cheking min and max value of the scaling_column
+print(dataset[scaling_column].describe().T[['min','max']])
