313682612efcc153853839fb057544179038757e
kevinadhiguna <kevin26798@gmail.com>
2021-02-23 11:50:28 +0700

    feat: add a python script

---
+++ Machine Learning with Python for Beginner/25.tugas-praktek-4.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+# splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+# Call the classifier
+decision_tree = DecisionTreeClassifier()
+# Fit the classifier to the training data
+decision_tree = decision_tree.fit(X_train, y_train)
+
+# evaluating the decision_tree performance
+print('Training Accuracy :', decision_tree.score(X_train, y_train))
+print('Testing Accuracy :', decision_tree.score(X_test, y_test))
