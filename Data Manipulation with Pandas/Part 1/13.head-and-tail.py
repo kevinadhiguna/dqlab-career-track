44e7bca578e04aa2732d665033265d6aafc34aec
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-03-22 04:44:55 +0000

    refactor: change dataset URL

--- Data Manipulation with Pandas/Part 1/13.head-and-tail.py
+++ Data Manipulation with Pandas/Part 1/13.head-and-tail.py
@@ -1,6 +1,6 @@
 import pandas as pd
 # Baca file sample_csv.csv
-df = pd.read_csv("https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/sample_csv.csv")
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_csv.csv")
 # Tampilkan 3 data teratas
 print("Tiga data teratas:\n", df.head(3))
 # Tampilkan 3 data terbawah
